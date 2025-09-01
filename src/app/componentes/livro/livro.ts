3186d79ec9b86fb1e7bb192c20e08d2cf1def7d7
Th Guasti <107655417+Th-Guasti@users.noreply.github.com>
2025-09-01 10:37:24 -0300

    eventos com o event binding

--- src/app/componentes/livro/livro.ts
+++ src/app/componentes/livro/livro.ts
@@ -7,9 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './livro.css'
 })
 export class Livro {
+  alternarFavorito() {
+    this.livro.favorito = !this.livro.favorito;
+  }
+
   livro = {
     titulo: "As ondas",
     autoria: "Virginia Woolf",
+    favorito: false,
     imagem: "https://m.media-amazon.com/images/I/81D2d6LqZdS._UF1000,1000_QL80_.jpg"
   }
 }
